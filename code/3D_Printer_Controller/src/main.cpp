b8a8acca5521a417fc01fdbb4c02b97d8679dea0
trachvik <vitrachta@gmail.com>
2025-09-24 18:39:50 +0200

    softAP

--- code/3D_Printer_Controller/src/main.cpp
+++ code/3D_Printer_Controller/src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "MyESP32AP";
+const char *password = "testpassword";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(112500);
+  WiFi.softAP(ssid, password);
+  
+  Serial.println();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
